a064838f37ce46fca71513bda67c436f3f2ca874
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-05-12 23:09:57 +0100

    Added Child name in registration form and updated table by adding child name

--- project/registration.php
+++ project/registration.php
@@ -1,184 +1,220 @@
 <?php
 include "header.php";
 ?>
-  <?php
-      if ($_SERVER['REQUEST_METHOD'] == "POST") {
-              if($_POST['Day']=="fullDay"){  //to get the number of days chosen from drop down menu inside show and hide function
-            		$time = $_POST["time1"];
-            	}
-              elseif ($_POST['Day']=="halfDay") {
-            		$time = $_POST["time2"];
-            	}
-              else{
-              }
-              //checking if any of the values are empty
-              if(empty($_POST['babyType'])){
-            		$errors[] = "Baby type is required";
-            	}
-              if(empty($_POST['Day'])){
-            		$errors[] = "Full day or Half Day type is required";
-            	}
-              if(empty($time)){
-                $errors[] = "Number of days is required";
-              }
-              if(empty($_POST['userName'])){
-            		$errors[] = "User Name is required";
-            	}
-              if(empty($_POST['email'])){
-            		$errors[] = "Email is required";
-            	}
-              else {
-                $email = trim($_POST['email']);
-                $regex = '/^[^0-9][_a-z0-9-]+(\.[_a-z0-9-]+)@[a-z0-9-]+(\.[a-z0-9-]+)(\.[a-z]{2,3})$/';
-                if (!preg_match($regex , $email)) { //matching the pattern
-                    $errors[] = "Invalid Email Address";
-              }
-              }
-              if(empty($_POST['password'])){
-            		$errors[] = "Password is required";
-            	}
+<!doctype html>
+<html lang="en">
+  <head>
+  <link rel="icon" type="image/png" href="images/logo_transparent.png"/>
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- <!- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-              if (empty($errors)) { //when there is zero errors
-                require "includes/mysql_connect.php";
-                // echo "File added Successfully";
-                if (isset($_POST['add'])){
-                  function validate($data){ //to minimize the error while entering value by the user
-          				      $data = trim($data);
-          				      $data = stripslashes($data);
-          				      $data = htmlspecialchars($data);
-          				      return $data;
-          			    }
-                if($_POST['Day']=="fullDay"){  //to get the number of days chosen from drop down menu inside show and hide function
-              		$time = $_POST["time1"];
-              	}
-                elseif ($_POST['Day']=="halfDay") {
-              		$time = $_POST["time2"];
-              	}
-                else{
-                }
-                $babyType = validate($_POST['babyType']);
-            		$day = validate($_POST['Day']);
-           	 		$userName = validate($_POST['userName']);
-                $email = validate($_POST['email']);
-                $password = validate($_POST['password']);
+    <link rel="stylesheet" href="Mystyle.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-                $to = 'thumamahahmed50@gmail.com, Leezapoudel01@gmail.com ' .$Email;
-                $subject = 'Registration Confirmation';
-                $message = 'Hi ' .$Name. ', '  . "\r\n" . 'Thank you for registration with kiddies Cove.
-                will follow up shortly.
-                you have chosen to register for ' . $time . ' , ' .  $babyType . ' , ' . $day . ' , ' . $time . '
-                your login details are the following :
-                username : ' .  $userName . '
-                Password : ' . $password . '
-                  '  . "\r\n" . '
-                Regards,
-                Kiddies Cove Head.';
-                $message = wordwrap($message, 70);
-                $headers = 'From: KiddiesCove@gmail.com' . "\r\n" . 'Reply-To: KiddiesCove@gmail.com' .
-                "\r\n". 'X-Mailer: PHP/' . phpversion();
-                // send
-                mail($to, $subject, $message, $headers);
 
-                $query = "SELECT * FROM child WHERE Email = '$email'"; //query to check if the email exsits in the table
+  </head>
+  <body>
+
+
+<?php
+ if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $errors = false;
+
+
+  if($_POST['Day']=="fullDay"){  //to get the number of days chosen from drop down menu inside show and hide function
+    $time = $_POST["time1"];
+  }
+  elseif ($_POST['Day']=="halfDay") {
+    $time = $_POST["time2"];
+  }
+  else{
+  }
+  //checking if any of the values are empty
+  if(empty($_POST['babyType'])){
+    $error= true;
+    $errors[] = "Baby type is required";
+  }
+  if(empty($_POST['Day'])){
+    $error= true;
+    $errors[] = "Full day or Half Day type is required";
+  }
+  if(empty($time)){
+    $error= true;
+    $errors[] = "Number of days is required";
+  }
+  if(empty($_POST['userName'])){
+    $errors[] = "User Name is required";
+  }
+  if(empty($_POST['childName'])){
+    $errors[] = "Child Name is required";
+  }
+  if(empty($_POST['email'])){
+    $error= true;
+    $errors[] = "Email is required";
+  }
+  else {
+    $email = trim($_POST['email']);
+    $regex = '/^[^0-9][_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
+    if (!preg_match($regex , $email)) { //matching the pattern
+      $error= true;
+        $errors[] = "Invalid Email Address";
+  }
+  }
+  if(empty($_POST['password'])){
+    $error= true;
+    $errors[] = "Password is required";
+  }
+
+  if (empty($errors)) { //when there is zero errors
+
+    function validate($data){ //to minimize the error while entering value by the user
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    if($_POST['Day']=="fullDay"){  //to get the number of days chosen from drop down menu inside show and hide function
+      $time = $_POST["time1"];
+    }
+    elseif ($_POST['Day']=="halfDay") {
+      $time = $_POST["time2"];
+    }
+    else{
+    }
+    $babyType = validate($_POST['babyType']);
+    $day = validate($_POST['Day']);
+    $userName = validate($_POST['userName']);
+    $childName = validate($_POST['childName']);
+    $email = validate($_POST['email']);
+    $password = validate($_POST['password']);
+
+    require "includes/mysql_connect.php";
+    $query = "SELECT * FROM child WHERE Email = '$email'"; //query to check if the email exsits in the table
           			$result = mysqli_query($conn, $query);
                 if(mysqli_num_rows($result) > 0) //if exists this will be executed
            	 		{
-           	 			echo "Data Exists";
+                  echo '<div class="succes-msg col-12 col-md-6">
+                  <h4>Email already exist</h4>
+
+              </div>';
            	 		}
                 else{
-                  $query1 = "INSERT INTO `child` (`Category`, `Day`, `Fee`, `UserName`, `Email`, `Password`) VALUES ('$babyType', '$day', '$time', '$userName', '$email', '$password');";
+                  $query1 = "INSERT INTO `child` (`Category`, `Day`, `Fee`, `UserName`, `ChildName`, `Email`, `Password`) VALUES ('$babyType', '$day', '$time', '$userName', '$childName', '$email', '$password');";
                   $insert_result = mysqli_query($conn, $query1);
-                  if ($insert_result) {
-                    echo "Child added Sucessfully";
-                  }else{
-                    echo "error";
-                    echo mysqli_connect_error($insert_result);
-                  }
+                  if ($insert_result){
+                    echo '<div class="succes-msg col-12 col-md-6">
+                            <h4> Registration Successfull </h4>
+                            <h4>well get back to you shortly </h4>
+                        </div>';
+
+                        $to = 'Leezapoudel01@gmail.com,' .$email;
+                        $subject = 'Registration Confirmation';
+                        $message = 'Hi ' .$Name. ', '  . "\r\n" . 'Thank you for registration with kiddies Cove.
+                        will follow up shortly.
+                        you have chosen to register for ' . $time . ' , ' .  $babyType . ' , ' . $day . ' , ' . $time . '
+                        your login details are the following :
+                        username : ' .  $userName . '
+                        Password : ' . $password . '
+                          '  . "\r\n" . '
+                        Regards,
+                        Kiddies Cove Head.';
+                        $message = wordwrap($message, 70);
+                        $headers = 'From: KiddiesCove@gmail.com' . "\r\n" . 'Reply-To: KiddiesCove@gmail.com' .
+                        "\r\n". 'X-Mailer: PHP/' . phpversion();
+                        // send
+                        mail($to, $subject, $message, $headers);
                 }
+                else{
+                    echo '<div class="succes-msg col-12 col-md-6">
+                            <h4>Message Not Sent </h4>
 
+                        </div>';
+                }
               }
-        }
-      }
-        else{ //printing out the errors if the value are not valid
-          foreach ($errors as $msg) {
-            echo "- $msg <br/>";
-          }
-        }
+  }
+}
+else{ //printing out the errors if the value are not valid
+  $errors[] = $err;
+}
 
+ ?>
+<?php
+if(!isset($_POST['add']) || (!empty($error)))
+{
   ?>
+ <div id="container2" class="container py-5 col-12 col-md-6">
+ <form id = "myform" action="registration.php" method="POST" class="main-form">
+          <h1> Child Registration Form </h1>
+          <p> Choose the category of your Child </p>
+          <select name = "babyType" id="babyType"  class="form-control"> //drop down menu for the baby type
+               <option value = "">--Select Baby Type--</option>
+               <option value = "baby"> Baby</option>
+               <option value = "wobbler"> Wobbler </option>
+               <option value = "toddler"> Toddler </option>
+               <option value = "preschool"> Preschool </option>
+            </select>
 
-  <!doctype html>
-  <html lang="en">
-    <head>
-    <link rel="icon" type="image/png" href="images/logo_transparent.png"/>
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="script.js">
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-
-    <!-- <!- Bootstrap CSS --> -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-      <link rel="stylesheet" href="Mystyle.css">
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-    </head>
-    <body>
-      <div id="container2" class="container py-5">
-        	<form id = "myform" action="registration.php" method="POST" class="main-form"> <!--//form created -->
-            <h1> Child Registration Form </h1>
-            <p> Choose the category of your Child </p>
-            <select name = "babyType" id="babyType"  class="form-control"> <!--//drop down menu for the baby type-->
-                 <option value = "">--Select Baby Type--</option>
-                 <option value = "baby"> Baby</option>
-                 <option value = "wobbler"> Wobbler </option>
-                 <option value = "toddler"> Toddler </option>
-                 <option value = "preschool"> Preschool </option>
+            <p> Choose the length of the day and the number of days </p>
+            <input type = "radio" name = "Day" id = "type1" required = "input" value = "fullDay" onclick="func();" />
+            <label for ="kid1"> Full day </label>
+            <fieldset id = "dayCareInfo1" class="reg-option">
+            <select name = "time1" id="time1" style="display:none" class="form-control">
+			           <option value = "">--Select Number of Days--</option>
+			           <option value = "49"> 1 day ($49)</option>
+			           <option value = "150"> 3 days ($147) </option>
+                 <option value = "250"> 5 days ($245) </option>
               </select>
+            </fieldset>
 
-              <p> Choose the length of the day and the number of days </p>
-              <input type = "radio" name = "Day" id = "type1" required = "input" value = "fullDay" onclick="func();" />  <!--//radio button to chose full day -->
-              <label for ="kid1"> Full day </label>
-              <fieldset id = "dayCareInfo1" class="reg-option">
-              <select name = "time1" id="time1" style="display:none" class="form-control">  <!--//drop down menu for number of days-->
+              <br>
+              <input type = "radio" name = "Day" id = "type2" required = "input" value = "halfDay" onclick="func();" />
+              <label for ="kid2"> Half day </label>
+              <fieldset id = "dayCareInfo2" class="reg-option">
+              <select name = "time2" id="time2" style="display:none" class="form-control">
   			           <option value = "">--Select Number of Days--</option>
   			           <option value = "49"> 1 day ($49)</option>
-  			           <option value = "147"> 3 days ($147) </option>
-                   <option value = "245"> 5 days ($245) </option>
+  			           <option value = "150"> 3 days ($147) </option>
+                   <option value = "250"> 5 days ($245) </option>
                 </select>
               </fieldset>
 
-                <br>
-                <input type = "radio" name = "Day" id = "type2" required = "input" value = "halfDay" onclick="func();" /> <!-- //radio button for half day-->
-                <label for ="kid2"> Half day </label>
-                <fieldset id = "dayCareInfo2" class="reg-option">
-                <select name = "time2" id="time2" style="display:none" class="form-control"><!--  //drop down menu for number of days-->
-    			           <option value = "">--Select Number of Days--</option>
-                     <option value = "30"> 1 day ($30)</option>
-    			           <option value = "90"> 3 days ($90) </option>
-                     <option value = "150"> 5 days ($150) </option>
-                  </select>
-                </fieldset>
+              <br>
+              UserName: <input type = "text" name = "userName" required="input"  value="" class="form-control"/>
+              ChildName: <input type = "text" name = "childName" required="input"  value="" class="form-control"/>
+              Email: <input type = "email" name = "email" required="input"  value=""  class="form-control"/>
+              Password: <input type = "password" name = "password" required="input"  value=""  class="form-control"/>
+              <input type = "hidden" name = "role" value="user" class="form-control"/>
+              <br>
+              <button type = "submit" name="add" class="btn btn-primary"> Submit </button>
 
-                <br>
-                UserName: <input type = "text" name = "userName" required="input"  value="" class="form-control"/> <!-- //field to enter user name-->
-                Email: <input type = "email" name = "email" required="input"  value=""  class="form-control"/> <!-- //field to enter email -->
-                Password: <input type = "password" name = "password" required="input"  value=""  class="form-control"/> <!-- //field to enter password -->
+               <?php
+                  if(isset($errors)){
+                    foreach($errors as $err){  // looping through errors
+                        echo "<h4><strong style='color:red;'>$err</strong></h4>"."<br>";
+                }
+              }
+                ?>
+              </form>
+              <?php
+}
+?>
 
-                <button type = "add" name="add" class="btn btn-primary"> Submit </button> <!-- //submit button -->
-                </form>
-            </div>
+</div>
 
- <?php
-   include "footer.html"
- ?>
- <script src="script.js"></script>
+<?php
+ include "footer.html"
+?>
+<script src="script.js">
+</script>
 
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 </body>
 </html>
--- project/tables.sql
+++ project/tables.sql
@@ -24,20 +24,20 @@ CREATE TABLE page (
 
 LOCK TABLES page WRITE;
 
-INSERT INTO page VALUES (1,'https://knuth.griffith.ie/~s3022041/project/images/pic5.jpg','New Activities',  'Our after school care is full of fun stuff, Arts & Crafts, Games, Music, Dance, Computers and also the space to unwind and relax with their friends or siblings. We know that modern life can get pretty hectic, and for busy parents, Kiddies can offer the flexibility to help out when you need extra support. For early morning starts we’ll provide a nutritious breakfast and can drop your child to school. We will also collect your child after school with a friendly face.', 'https://knuth.griffith.ie/~s3022041/project/login.php', 'join us know');
+INSERT INTO page VALUES (1,'https://knuth.griffith.ie/~s2999960/childcare-premise-php-project/project/images/pic5.jpg','New Activities',  'Our after school care is full of fun stuff, Arts & Crafts, Games, Music, Dance, Computers and also the space to unwind and relax with their friends or siblings. We know that modern life can get pretty hectic, and for busy parents, Kiddies can offer the flexibility to help out when you need extra support. For early morning starts we’ll provide a nutritious breakfast and can drop your child to school. We will also collect your child after school with a friendly face.', 'https://knuth.griffith.ie/~s2999960/childcare-premise-php-project/project/login.php', 'join us know');
 
 UNLOCK TABLES;
 
-INSERT INTO page VALUES (2,'https://knuth.griffith.ie/~s3022041/project/images/gif1.gif','Special Offers',  'We aim to provide quality, affordable childcare to families.  We provide special discounts to families of 2 or more children
+INSERT INTO page VALUES (2,'https://knuth.griffith.ie/~s2999960/childcare-premise-php-project/project/images/gif1.gif','Special Offers',  'We aim to provide quality, affordable childcare to families.  We provide special discounts to families of 2 or more children
                       availing of full time care. Please contact our accounts department for further details on our special family discount. At Links,
                       we operate a sibling programme where we encourage children from families of two of more to spend valuable time together throughout
                       the day. We understand how critically important the ‘sibling bond’ is at a young age.  The sibling relationship is one of the most
                       important relationships in a child’s life. We believe that sibling time, can enhance a child’s sense of safety and well-being and
                       create a home-away-from-home environment within the crèche. If you wish to avail of our special family discount offer please
-                      contact us at 1890 93 00 82.', 'https://knuth.griffith.ie/~s3022041/project/contactUs.php', 'Contact for more information');
+                      contact us at 1890 93 00 82.', 'https://knuth.griffith.ie/~s2999960/childcare-premise-php-project/project/contactUs.php', 'Contact for more information');
 
 
-INSERT INTO page VALUES (3,'https://knuth.griffith.ie/~s3022041/project/images/pic6.png','New Events',  'Each year a Graduation Ceremony is held for children graduating from our Early Childhood Care and Education (ECCE) classes.
+INSERT INTO page VALUES (3,'https://knuth.griffith.ie/~s2999960/childcare-premise-php-project/project/images/pic6.png','New Events',  'Each year a Graduation Ceremony is held for children graduating from our Early Childhood Care and Education (ECCE) classes.
                       Having spent several years in Links Childcare your child has embarked on their first steps on the road to developing as an
                        independent person. We believe it is important to recognise these very important steps by holding a Graduation Ceremony.
                        The graduation ceremony, marks another step in your child’s development as he/she transitions into ‘Big School’.  This event
@@ -45,7 +45,7 @@ INSERT INTO page VALUES (3,'https://knuth.griffith.ie/~s3022041/project/images/p
                        skills which they have acquired during their time with us. The graduation marks a very special occasion, it will possibly be the first of many graduation ceremonies which you will attend
                         with your child.  Each child receives their first educational parchment to mark their sense of achievement during their time at
                          Links Childcare.  The Ceremony celebrates children’s journey into “Big School” and gives them the opportunity to dress in their
-                         first cap and gown in front of their family and friends.', 'https://knuth.griffith.ie/~s3022041/project/registration.php', 'Register Now');
+                         first cap and gown in front of their family and friends.', 'https://knuth.griffith.ie/~s2999960/childcare-premise-php-project/project/registration.php', 'Register Now');
 
 
 
@@ -66,8 +66,10 @@ service_Name VARCHAR(35) NOT NULL,
 parent_name VARCHAR(50) NOT NULL,
 testi_date Date not null,
 comment VARCHAR(500) NOT NULL,
+display varchar(4) DEFAULT 'SHOW',
 PRIMARY KEY (testimonial_id)
 );
+
 
 CREATE TABLE service(
 service_id INT(12) NOT NULL AUTO_INCREMENT,
@@ -79,6 +81,7 @@ PRIMARY KEY (service_id)
 );
 
 
+
 INSERT INTO service VALUES (1,'', 'School Transportation', '', '');
 INSERT INTO service VALUES (2,'', 'Trained Supervision', '', '');
 INSERT INTO service VALUES (3,'', 'School Transportation', '', '');
@@ -88,13 +91,26 @@ INSERT INTO service VALUES (6,'', 'Lunch Service', '', '');
 INSERT INTO service VALUES (7,'', 'Evening sleeping bed', '', '');
 
 
--- created table for child registration
-CREATE TABLE child(
-ID INT PRIMARY KEY AUTO_INCREMENT,
-Category VARCHAR(100) NOT NULL,
-Day VARCHAR(100) NOT NULL,
-Fee INT (50) NOT NULL,
-UserName VARCHAR(100) NOT NULL,
-Email VARCHAR(100) NOT NULL UNIQUE,
-Password VARCHAR(100) NOT NULL
-);
+ -- created table for child registration
+ CREATE TABLE child(
+ ID INT PRIMARY KEY AUTO_INCREMENT,
+ Category VARCHAR(100) NOT NULL,
+ Day VARCHAR(100) NOT NULL,
+ Fee INT (50) NOT NULL,
+ Username VARCHAR(100) NOT NULL,
+ Childname VARCHAR(100) NOT NULL,
+ Email VARCHAR(100) NOT NULL UNIQUE,
+ role varchar(4) DEFAULT 'user',
+ password VARCHAR(100) NOT NULL
+ );
+
+
+ CREATE TABLE day(
+ ID INT PRIMARY KEY AUTO_INCREMENT,
+ date Date NOT NULL,
+ name VARCHAR(100) NOT NULL,
+ temperature INT (100) NOT NULL,
+ breakfast VARCHAR(100) NOT NULL,
+ lunch VARCHAR(100) NOT NULL,
+ activities varchar(100) NOT NULL
+ );
