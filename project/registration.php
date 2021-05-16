5aa3efbbdddcdf068bb022f8507879f987160f41
Leezazaazaaa <leeza.poudel@student.griffith.ie>
2021-05-16 20:03:20 +0100

    Removed the silly mistake

--- project/registration.php
+++ project/registration.php
@@ -1,240 +1,220 @@
 <?php
-session_start();
-    if(!isset($_SESSION['Admin'])){
-        header("Location: ../login.php");
-    }
-	?>
-
-<!DOCTYPE html>
+include "header.php";
+?>
+<!doctype html>
 <html lang="en">
+  <head>
+  <link rel="icon" type="image/png" href="images/logo_transparent.png"/>
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- <!- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-<head>
-
+    <link rel="stylesheet" href="Mystyle.css">
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Kiddies Cove</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  </head>
+  <body>
 
-</head>
-
-<body id="page-top">
 
 <?php
-    include "sidebar.php";
-   ?>
+ if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $errors = false;
 
-<?php
-require "../includes/mysql_connect.php";
-$select_query = "Select * from fee";
-$select_result = mysqli_query($conn, $select_query);
 
-if (mysqli_num_rows($select_result) > 0) {
-  // output data of each row
-  $feeArr = array();
-  while($row = mysqli_fetch_assoc($select_result)) {
-    array_push($feeArr, $row['Fee']);
-    // $Full1Day = $row[0]['Fee'];
-    // $Full3Day = $row[1]['Fee'];
-    // $Full5Day = $row[2]['Fee'];
-    // $Half1Day = $row[3]['Fee'];
-    // $Half3Day = $row[4]['Fee'];
-    // $Half5Day = $row[5]['Fee'];
+  if($_POST['Day']=="fullDay"){  //to get the number of days chosen from drop down menu inside show and hide function
+    $time = $_POST["time1"];
   }
-} else {
+  elseif ($_POST['Day']=="halfDay") {
+    $time = $_POST["time2"];
+  }
+  else{
+  }
+  //checking if any of the values are empty
+  if(empty($_POST['babyType'])){
+    $error= true;
+    $errors[] = "Baby type is required";
+  }
+  if(empty($_POST['Day'])){
+    $error= true;
+    $errors[] = "Full day or Half Day type is required";
+  }
+  if(empty($time)){
+    $error= true;
+    $errors[] = "Number of days is required";
+  }
+  if(empty($_POST['userName'])){
+    $errors[] = "User Name is required";
+  }
+  if(empty($_POST['childName'])){
+    $errors[] = "Child Name is required";
+  }
+  if(empty($_POST['email'])){
+    $error= true;
+    $errors[] = "Email is required";
+  }
+  else {
+    $email = trim($_POST['email']);
+    $regex = '/^[^0-9][_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
+    if (!preg_match($regex , $email)) { //matching the pattern
+      $error= true;
+        $errors[] = "Invalid Email Address";
+  }
+  }
+  if(empty($_POST['password'])){
+    $error= true;
+    $errors[] = "Password is required";
+  }
 
+  if (empty($errors)) { //when there is zero errors
+
+    function validate($data){ //to minimize the error while entering value by the user
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+    if($_POST['Day']=="fullDay"){  //to get the number of days chosen from drop down menu inside show and hide function
+      $time = $_POST["time1"];
+    }
+    elseif ($_POST['Day']=="halfDay") {
+      $time = $_POST["time2"];
+    }
+    else{
+    }
+    $babyType = validate($_POST['babyType']);
+    $day = validate($_POST['Day']);
+    $userName = validate($_POST['userName']);
+    $childName = validate($_POST['childName']);
+    $email = validate($_POST['email']);
+    $password = validate($_POST['password']);
+
+    require "includes/mysql_connect.php";
+    $query = "SELECT * FROM child WHERE Email = '$email'"; //query to check if the email exsits in the table
+          			$result = mysqli_query($conn, $query);
+                if(mysqli_num_rows($result) > 0) //if exists this will be executed
+           	 		{
+                  echo '<div class="succes-msg col-12 col-md-6">
+                  <h4>Email already exist</h4>
+
+              </div>';
+           	 		}
+                else{
+                  $query1 = "INSERT INTO `child` (`Category`, `Day`, `FeeID`, `UserName`, `ChildName`, `Email`, `Password`) VALUES ('$babyType', '$day', '$time', '$userName', '$childName', '$email', '$password');";
+                  $insert_result = mysqli_query($conn, $query1);
+                  if ($insert_result){
+                    echo '<div class="succes-msg col-12 col-md-6">
+                            <h4> Registration Successfull </h4>
+                            <h4>well get back to you shortly </h4>
+                        </div>';
+
+                        $to = 'Leezapoudel01@gmail.com,' .$email;
+                        $subject = 'Registration Confirmation';
+                        $message = 'Hi ' .$Name. ', '  . "\r\n" . 'Thank you for registration with kiddies Cove.
+                        will follow up shortly.
+                        you have chosen to register for ' . $time . ' , ' .  $babyType . ' , ' . $day . ' , ' . $time . '
+                        your login details are the following :
+                        username : ' .  $userName . '
+                        Password : ' . $password . '
+                          '  . "\r\n" . '
+                        Regards,
+                        Kiddies Cove Head.';
+                        $message = wordwrap($message, 70);
+                        $headers = 'From: KiddiesCove@gmail.com' . "\r\n" . 'Reply-To: KiddiesCove@gmail.com' .
+                        "\r\n". 'X-Mailer: PHP/' . phpversion();
+                        // send
+                        mail($to, $subject, $message, $headers);
+                }
+                else{
+                    echo '<div class="succes-msg col-12 col-md-6">
+                            <h4>Message Not Sent </h4>
+
+                        </div>';
+                }
+              }
+  }
 }
-  echo "<div id='container2' class='container py-5 col-12 col-md-6'>
-  <form id = 'myform' action='registration_edit.php' method='POST' class='main-form'>
-           <h1> Child Registration Form </h1>
-           <p> Choose the category of your Child </p>
-             <p> Choose the length of the day and the number of days </p>
-             <label for ='kid1'> Full day </label>
-             <div class='row'>
-                <div class='col'>1 Day:</div>
-                <div class='col'> <input type = 'number' name = 'f1day' required='input'  value='$feeArr[0]' class='form-control'/></div>
-                <div class='col'>3 Day:</div>
-                <div class='col'> <input type = 'number' name = 'f3day' required='input'  value='$feeArr[1]' class='form-control'/></div>
-                <div class='col'>5 Day:</div>
-                <div class='col'> <input type = 'number' name = 'f5day' required='input'  value='$feeArr[2]' class='form-control'/></div>
-              </div>
-               <br>
+else{ //printing out the errors if the value are not valid
+  $errors[] = $err;
+}
 
-               <label for ='kid2'> Half day </label>
-               <div class='row'>
-                  <div class='col'>1 Day:</div>
-                  <div class='col'> <input type = 'number' name = 'h1day' required='input'  value='$feeArr[3]' class='form-control'/></div>
-                  <div class='col'>3 Day:</div>
-                  <div class='col'> <input type = 'number' name = 'h3day' required='input'  value='$feeArr[4]' class='form-control'/></div>
-                  <div class='col'>5 Day:</div>
-                  <div class='col'> <input type = 'number' name = 'h5day' required='input'  value='$feeArr[5]' class='form-control'/></div>
-                </div>
-               <br>
-               <button type = 'submit' name='add' class='btn btn-primary' onclick='reload();'> Submit </button>
-               </form>";
+ ?>
+<?php
+if(!isset($_POST['add']) || (!empty($error)))
+{
+  ?>
+ <div id="container2" class="container py-5 col-12 col-md-6">
+ <form id = "myform" action="registration.php" method="POST" class="main-form">
+          <h1> Child Registration Form </h1>
+          <p> Choose the category of your Child </p>
+          <select name = "babyType" id="babyType"  class="form-control"> //drop down menu for the baby type
+               <option value = "">--Select Baby Type--</option>
+               <option value = "baby"> Baby</option>
+               <option value = "wobbler"> Wobbler </option>
+               <option value = "toddler"> Toddler </option>
+               <option value = "preschool"> Preschool </option>
+            </select>
 
-        // using the require function to call the connection
+            <p> Choose the length of the day and the number of days </p>
+            <input type = "radio" name = "Day" id = "type1" required = "input" value = "fullDay" onclick="func();" />
+            <label for ="kid1"> Full day </label>
+            <fieldset id = "dayCareInfo1" class="reg-option">
+            <select name = "time1" id="time1" style="display:none" class="form-control">
+			           <option value = "">--Select Number of Days--</option>
+			           <option value = "49"> 1 day ($49)</option>
+			           <option value = "150"> 3 days ($147) </option>
+                 <option value = "250"> 5 days ($245) </option>
+              </select>
+            </fieldset>
 
+              <br>
+              <input type = "radio" name = "Day" id = "type2" required = "input" value = "halfDay" onclick="func();" />
+              <label for ="kid2"> Half day </label>
+              <fieldset id = "dayCareInfo2" class="reg-option">
+              <select name = "time2" id="time2" style="display:none" class="form-control">
+  			           <option value = "">--Select Number of Days--</option>
+  			           <option value = "49"> 1 day ($49)</option>
+  			           <option value = "150"> 3 days ($147) </option>
+                   <option value = "250"> 5 days ($245) </option>
+                </select>
+              </fieldset>
 
-         if(isset($_POST['add']))
-         {
-         $Full1Day= $feeArr[0]= $_POST['f1day'];
-         $Full3Day= $feeArr[1] = $_POST['f3day'];
-         $Full5Day= $feeArr[2] = $_POST['f5day'];
-         $Half1Day= $feeArr[3] = $_POST['h1day'];
-         $Half3Day= $feeArr[4] = $_POST['h3day'];
-         $Half5Day= $feeArr[5] = $_POST['h5day'];
+              <br>
+              UserName: <input type = "text" name = "userName" required="input"  value="" class="form-control"/>
+              ChildName: <input type = "text" name = "childName" required="input"  value="" class="form-control"/>
+              Email: <input type = "email" name = "email" required="input"  value=""  class="form-control"/>
+              Password: <input type = "password" name = "password" required="input"  value=""  class="form-control"/>
+              <input type = "hidden" name = "role" value="user" class="form-control"/>
+              <br>
+              <button type = "submit" name="add" class="btn btn-primary"> Submit </button>
 
-         // echo  $babyType;
-         // echo  $Full1Day;
-         // echo  $Full3Day;
-         // echo  $Full5Day ;
-         // echo  $Half1Day;
-         // echo  $Half3Day;
-         // echo  $Half5Day;
-         if($_SERVER['REQUEST_METHOD'] == "POST"){
-           $errors = false;
-           if(empty($_POST['f1day'])){
-             $error= true;
-             $errors[] = "1 day full time is required";
-           }
+               <?php
+                  if(isset($errors)){
+                    foreach($errors as $err){  // looping through errors
+                        echo "<h4><strong style='color:red;'>$err</strong></h4>"."<br>";
+                }
+              }
+                ?>
+              </form>
+              <?php
+}
+?>
 
-           if(empty($_POST['f3day'])){
-             $error= true;
-             $errors[] = "3 days full time fee is required";
-           }
+</div>
 
-           if(empty($_POST['f5day'])){
-             $error= true;
-             $errors[] = "5 days full time fee is required";
-           }
-
-           if(empty($_POST['h1day'])){
-             $error= true;
-             $errors[] = "1 day half time fee is required";
-           }
-
-           if(empty($_POST['h3day'])){
-             $error= true;
-             $errors[] = "3 day half time fee is required";
-           }
-
-           if(empty($_POST['h5day'])){
-             $error= true;
-             $errors[] = "5 day half time fee is required";
-           }
-
-           if (empty($errors)) {
-             function validate($data){ //to minimize the error while entering value by the user
-               $data = trim($data);
-               $data = stripslashes($data);
-               $data = htmlspecialchars($data);
-               return $data;
-             }
-
-             $Full1Day = validate($_POST['f1day']);
-             $Full3Day = validate($_POST['f3day']);
-             $Full5Day = validate($_POST['f5day']);
-             $Half1Day = validate($_POST['h1day']);
-             $Half3Day = validate($_POST['h3day']);
-             $Half5Day = validate($_POST['h5day']);
+<?php
+ include "footer.html"
+?>
+<script src="script.js">
+</script>
 
 
-             // require("../includes/mysql_connect.php");
-             $delete_query = "DELETE FROM `fee`";
-             $delete_result = mysqli_query($conn, $delete_query);
-             $insert_query = "INSERT INTO  `fee`(`ID`,`Fee`, `Day`, `Duration`)
-             VALUES
-             (1, '$Full1Day', 'fullDay', '1'),
-             (2, '$Full3Day', 'fullDay', '3'),
-             (3, '$Full5Day', 'fullDay', '5'),
-             (4, '$Half1Day', 'halfDay', '1'),
-             (5, '$Half3Day', 'halfDay', '3'),
-             (6, '$Half5Day', 'halfDay', '5')";
-             $insert_result = mysqli_query($conn, $insert_query);
-           }
-         }
-
-       }
-
-
-	?>
-
-
-
-
-
-
-
-                </div>
-                <!-- /.container-fluid -->
-
-            </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
-        </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="../script.js"></script>
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 </body>
-
 </html>
